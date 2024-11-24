41aa00071f00a912100101f7e4e94d096b543ca4
mac <mac@MacBook-Pro-M4.local>
2024-11-24 14:18:17 +0300

    Рефакторинг в соответствии с правками первого ревью

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,9 +1,143 @@
-import { ReactNode } from 'react';
+import { useState } from 'react';
+
+import { ArrowButton } from 'src/ui/arrow-button';
+import { Button } from 'src/ui/button';
+import cn from 'classnames';
+
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+
+import styles from 'src/components/article-params-form/ArticleParamsForm.module.scss';
+
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 
 type TArticleParamsForm = {
-	children: ReactNode;
+	isOpen: boolean;
+	onClick: () => void;
+	onSubmit: (params?: any) => void;
 };
 
-export const ArticleParamsForm = ({ children }: TArticleParamsForm) => {
-	return <>{children}</>;
+export const ArticleParamsForm = ({
+	isOpen,
+	onClick,
+	onSubmit,
+}: TArticleParamsForm) => {
+	//Состояние формы
+	const [formState, setFormState] = useState(defaultArticleState);
+
+	// Универсальная функция изменения полей формы
+	const handleSetFormState = <
+		K extends keyof ArticleStateType,
+		V = ArticleStateType[K]
+	>(
+		inputName: K,
+		value: V
+	) => {
+		setFormState((prev) => {
+			return {
+				...prev,
+				[inputName]: value,
+			};
+		});
+	};
+
+	// Применение настроек к статье
+	const handleSubmit = () => {
+		onSubmit(formState);
+	};
+
+	// Сброс настроек
+	const handleCleare = () => {
+		onSubmit(defaultArticleState);
+	};
+
+	return (
+		<>
+			<ArrowButton isOpen={isOpen} onClick={onClick} />
+			<aside
+				className={cn(styles.container, isOpen ? styles.container_open : null)}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => {
+						e.preventDefault();
+						handleSubmit();
+					}}>
+					{/* Заголовок формы */}
+					<Text as='h2' size={31} weight={800} uppercase={true}>
+						задайте параметры
+					</Text>
+					{/* Дропдаун "шрифт" */}
+					<Select
+						title={'шрифт'}
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(selected) => {
+							handleSetFormState('fontFamilyOption', selected);
+						}}
+					/>
+					{/* Радиогруппа "размер шрифта" */}
+					<RadioGroup
+						title={'размер шрифта'}
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						name={'radio'}
+						onChange={(value) => {
+							handleSetFormState('fontSizeOption', value);
+						}}
+					/>
+					{/* Дропдаун "цвет шрифта" */}
+					<Select
+						title={'цвет шрифта'}
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(selected) => {
+							handleSetFormState('fontColor', selected);
+						}}
+					/>
+					{/* Сепаратор */}
+					<Separator />
+					{/* Дропдаун "цвет фона" */}
+					<Select
+						title={'цвет фона'}
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(selected) => {
+							handleSetFormState('backgroundColor', selected);
+						}}
+					/>
+					{/* Дропдаун "ширина контента" */}
+					<Select
+						title={'ширина контента'}
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(selected) => {
+							handleSetFormState('contentWidth', selected);
+						}}
+					/>
+					<div className={styles.bottomContainer}>
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={() => {
+								setFormState(defaultArticleState);
+								handleCleare();
+							}}
+						/>
+						<Button title='Применить' htmlType='submit' type='apply' />
+					</div>
+				</form>
+			</aside>
+		</>
+	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -8,40 +8,20 @@ import { ArticleParamsForm } from './components/article-params-form/ArticleParam
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
-import { ArrowButton } from 'src/ui/arrow-button';
-import { Button } from 'src/ui/button';
-import cn from 'classnames';
-
-import { Text } from 'src/ui/text';
-import { Select } from 'src/ui/select';
-import { RadioGroup } from 'src/ui/radio-group';
-import { Separator } from 'src/ui/separator';
-
-import stylesParamForm from 'src/components/article-params-form/ArticleParamsForm.module.scss';
-
-import {
-	fontFamilyOptions,
-	fontSizeOptions,
-	fontColors,
-	backgroundColors,
-	contentWidthArr,
-	defaultArticleState,
-} from 'src/constants/articleProps';
-
 import { useOutsideClickClose } from './ui/select/hooks/useOutsideClickClose';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
-// Объект параметров для статьи
-let paramsForArticle = structuredClone(defaultArticleState);
+import { defaultArticleState } from 'src/constants/articleProps';
 
 const App = () => {
 	// Ссылка на обёртку формы
-	const rootRef = useRef(null);
+	const rootFormRef = useRef(null);
 
-	// Состояние тригера кнопок
-	const [btnClickState, setBtnClickState] = useState(false);
+	// Состояние параметров статьи
+	const [paramsForArticleState, setParamsForArticle] =
+		useState(defaultArticleState);
 
 	// Состояние стрелки
 	const [arrowState, setArrowState] = useState(false);
@@ -49,7 +29,7 @@ const App = () => {
 	// Закрытие вне стрелки
 	useOutsideClickClose({
 		isOpen: arrowState,
-		rootRef: rootRef,
+		rootRef: rootFormRef,
 		onClose: () => {
 			setArrowState(false);
 		},
@@ -61,143 +41,33 @@ const App = () => {
 	// Функиця изменения состояния стрелки
 	const handleArrowState = () => setArrowState(!arrowState);
 
-	// Состояние дропдауна "шрифт"
-	const [fontFamilyOptionState, setFamilyOptionState] = useState(
-		fontFamilyOptions[0]
-	);
-
-	// Состояние радиогруппы "размер шрифта"
-	const [fontSizeOptionState, setFontSizeOptionState] = useState(
-		fontSizeOptions[0]
-	);
-
-	// Состояние дропдауна "размер шрифта"
-	const [fontColorState, setFontColorState] = useState(fontColors[0]);
-
-	// Состояние дропдауна "цвет шрифта"
-	const [backgroundColorState, setBackgroundColorState] = useState(
-		backgroundColors[0]
-	);
-
-	// Состояние дропдауна "ширина контента"
-	const [contentWidthArrState, setContentWidthArrState] = useState(
-		contentWidthArr[0]
-	);
-
-	// Применение настроек к статье
-	useEffect(() => {
-		setFamilyOptionState(paramsForArticle.fontFamilyOption);
-		setFontSizeOptionState(paramsForArticle.fontSizeOption);
-		setFontColorState(paramsForArticle.fontColor);
-		setBackgroundColorState(paramsForArticle.backgroundColor);
-		setContentWidthArrState(paramsForArticle.contentWidth);
-	}, [btnClickState]);
+	// Функция применения параметров к статье
+	const handleParamsForArticle = (params: any) => {
+		setParamsForArticle(params);
+	};
 
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': paramsForArticle.fontFamilyOption.value,
-					'--font-size': paramsForArticle.fontSizeOption.value,
-					'--font-color': paramsForArticle.fontColor.value,
-					'--container-width': paramsForArticle.contentWidth.value,
-					'--bg-color': paramsForArticle.backgroundColor.value,
+					'--font-family': paramsForArticleState.fontFamilyOption.value,
+					'--font-size': paramsForArticleState.fontSizeOption.value,
+					'--font-color': paramsForArticleState.fontColor.value,
+					'--container-width': paramsForArticleState.contentWidth.value,
+					'--bg-color': paramsForArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<div ref={rootRef}>
-				<ArticleParamsForm>
-					<>
-						<ArrowButton isOpen={arrowState} onClick={handleArrowState} />
-						<aside
-							className={cn(
-								stylesParamForm.container,
-								arrowState ? stylesParamForm.container_open : null
-							)}>
-							<form className={stylesParamForm.form}>
-								{/* Заголовок формы */}
-								<Text as='h1' size={31} weight={800} uppercase={true}>
-									задайте параметры
-								</Text>
-								{/* Дропдаун "шрифт" */}
-								<Select
-									title={'шрифт'}
-									selected={fontFamilyOptionState}
-									options={fontFamilyOptions}
-									onChange={(selected) => {
-										setFamilyOptionState(selected);
-									}}
-								/>
-								{/* Радиогруппа "размер шрифта" */}
-								<RadioGroup
-									title={'размер шрифта'}
-									selected={fontSizeOptionState}
-									options={fontSizeOptions}
-									name={'radio'}
-									onChange={(value) => {
-										setFontSizeOptionState(value);
-									}}
-								/>
-								{/* Дропдаун "цвет шрифта" */}
-								<Select
-									title={'цвет шрифта'}
-									selected={fontColorState}
-									options={fontColors}
-									onChange={(selected) => {
-										setFontColorState(selected);
-									}}
-								/>
-								{/* Сепаратор */}
-								<Separator />
-								{/* Дропдаун "цвет фона" */}
-								<Select
-									title={'цвет фона'}
-									selected={backgroundColorState}
-									options={backgroundColors}
-									onChange={(selected) => {
-										setBackgroundColorState(selected);
-									}}
-								/>
-								{/* Дропдаун "ширина контента" */}
-								<Select
-									title={'ширина контента'}
-									selected={contentWidthArrState}
-									options={contentWidthArr}
-									onChange={(selected) => {
-										setContentWidthArrState(selected);
-									}}
-								/>
-								<div className={stylesParamForm.bottomContainer}>
-									<Button
-										title='Сбросить'
-										htmlType='reset'
-										type='clear'
-										onClick={() => {
-											paramsForArticle = structuredClone(defaultArticleState);
-											setBtnClickState(!btnClickState);
-										}}
-									/>
-									<Button
-										title='Применить'
-										htmlType='submit'
-										type='apply'
-										onClick={(e) => {
-											e.preventDefault();
-											paramsForArticle.fontFamilyOption = fontFamilyOptionState;
-											paramsForArticle.fontSizeOption = fontSizeOptionState;
-											paramsForArticle.fontColor = fontColorState;
-											paramsForArticle.backgroundColor = backgroundColorState;
-											paramsForArticle.contentWidth = contentWidthArrState;
-											setBtnClickState(!btnClickState);
-										}}
-									/>
-								</div>
-							</form>
-						</aside>
-					</>
-				</ArticleParamsForm>
+			<div ref={rootFormRef}>
+				<ArticleParamsForm
+					isOpen={arrowState}
+					onClick={handleArrowState}
+					onSubmit={handleParamsForArticle}
+				/>
 			</div>
-			<Article />
+			<div>
+				<Article />
+			</div>
 		</main>
 	);
 };
