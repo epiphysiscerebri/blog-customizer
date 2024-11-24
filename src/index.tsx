8859351f8ce234e642798633b8f616ff855c447e
mac <mac@MacBook-Pro-M4.local>
2024-11-24 14:27:57 +0300

    Перенёс App.tsx в отдельный файл

--- src/index.tsx
+++ src/index.tsx
@@ -1,76 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState, useEffect, useRef } from 'react';
-import clsx from 'clsx';
+import { StrictMode } from 'react';
 
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
-
-import { useOutsideClickClose } from './ui/select/hooks/useOutsideClickClose';
+import { App } from './App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-import { defaultArticleState } from 'src/constants/articleProps';
-
-const App = () => {
-	// Ссылка на обёртку формы
-	const rootFormRef = useRef(null);
-
-	// Состояние параметров статьи
-	const [paramsForArticleState, setParamsForArticle] =
-		useState(defaultArticleState);
-
-	// Состояние стрелки
-	const [arrowState, setArrowState] = useState(false);
-
-	// Закрытие вне стрелки
-	useOutsideClickClose({
-		isOpen: arrowState,
-		rootRef: rootFormRef,
-		onClose: () => {
-			setArrowState(false);
-		},
-		onChange: () => {
-			arrowState;
-		},
-	});
-
-	// Функиця изменения состояния стрелки
-	const handleArrowState = () => setArrowState(!arrowState);
-
-	// Функция применения параметров к статье
-	const handleParamsForArticle = (params: any) => {
-		setParamsForArticle(params);
-	};
-
-	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': paramsForArticleState.fontFamilyOption.value,
-					'--font-size': paramsForArticleState.fontSizeOption.value,
-					'--font-color': paramsForArticleState.fontColor.value,
-					'--container-width': paramsForArticleState.contentWidth.value,
-					'--bg-color': paramsForArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<div ref={rootFormRef}>
-				<ArticleParamsForm
-					isOpen={arrowState}
-					onClick={handleArrowState}
-					onSubmit={handleParamsForArticle}
-				/>
-			</div>
-			<div>
-				<Article />
-			</div>
-		</main>
-	);
-};
 
 root.render(
 	<StrictMode>
